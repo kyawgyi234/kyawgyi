31be160a1a68eb20c090bd9a194f9fca430461c1
kyawgyi234 <68673982+kyawgyi234@users.noreply.github.com>
2020-10-12 20:06:09 +0630

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include("simple_html_dom.php");
-    $html=file_get_html("https://www.myanmarmoviestore.com/dear-dakanda-2005");
-    $title=$html->find("a#tracking-url",0)->innertext;
-    echo $title;
-    ?>
